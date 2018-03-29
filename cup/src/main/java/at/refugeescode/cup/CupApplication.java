f5bc2e72d85e5173f166e6b9ab3c22fa3e2088bc
timaf <lo_sa_na@yahoo.com>
2018-03-29 13:10:32 +0200

    start

---
+++ cup/src/main/java/at/refugeescode/cup/CupApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.cup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupApplication.class, args);
+	}
+}
